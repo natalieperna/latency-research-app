3d09b17ca46947ed56db21125e3024e0e6ff5338
Natalie Perna <nmperna@gmail.com>
2014-12-13 23:11:03 -0500

    Method for calculating accuracy of path.

--- processor/TestResults.java
+++ processor/TestResults.java
@@ -1,10 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class TestResults {
 	int test;
 	int lag;
 	long time;
 	ArrayList<TimePoint> path;
+	
+	// Circle
+	float cx = 568;
+	float cy = 600;
+	float r = 395;
+	
+	public double accuracy() {
+		ArrayList<Double> distances = new ArrayList<Double>();
+		for (TimePoint point : path) {
+			distances.add(distanceToCircle(point.x, point.y));
+		}
+		return averageOfList(distances);
+	}
+	
+	private double distanceToCircle(float x, float y) {
+		return Math.abs(Math.sqrt((x - cx)*(x - cx) + (y - cy)*(y - cy))-r);
+	}
+	
+	private double averageOfList(Collection<Double> values) {
+		Double sum = 0.0;
+		for (Double value : values) {
+			sum += value;
+		}
+		return sum.doubleValue() / values.size();
+	}
 
 	@Override
 	public String toString() {
